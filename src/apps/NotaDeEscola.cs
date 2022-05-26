895e201fa91c807113e400e8c140271c45428fc6
GabriewF <feegasaa@gmail.com>
2022-05-26 13:26:47 -0300

    3 Commit

--- src/apps/NotaDeEscola.cs
+++ src/apps/NotaDeEscola.cs
@@ -1,9 +1,17 @@
 namespace Rhamoncoders {
   class School {
     public static void Run() {
+      Console.Clear();
       Console.WriteLine("Qual o seu nome?");
       var name = Console.ReadLine();
-      
+
+      Console.WriteLine($"{name}, digite até que numero você quer que conte automaticamente");
+      var number = Convert.ToInt64(Console.ReadLine());
+
+      for (var i = 1; i < number; i++) {
+        Console.WriteLine(i);
+      }
+
       Console.WriteLine();
       Console.ForegroundColor = ConsoleColor.Red;
       Console.WriteLine("Saindo em 5 Segundos...");
